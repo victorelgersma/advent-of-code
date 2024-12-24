29979a73afe4ba86af88cd6ec1d729a5bfa1a702
Victor Elgersma <victor.elgersma41@thg.com>
2024-12-24 09:06:31 +0000

    part 2 complete

--- lua/2024/day/1/solution.lua
+++ lua/2024/day/1/solution.lua
@@ -1,7 +1,6 @@
 -- usage: cat input | lua solution.lua
 
 
-
 -- read from standard input
 -- populate two tables
 
@@ -9,10 +8,10 @@ table1= {}
 table2= {}
 
 for line in io.lines() do
-    local word1 = string.sub(line, 1, 5)
-    local word2 = string.sub(line, 8, 13)
-    table.insert(table1, tonumber(word1))
-    table.insert(table2, tonumber(word2))
+    local left = string.sub(line, 1, 5)
+    local right = string.sub(line, 8, 13)
+    table.insert(table1, tonumber(left))
+    table.insert(table2, tonumber(right))
 end
 
 -- sort two tables separately
@@ -31,8 +30,6 @@ for i=1, maxLength do
     table.insert(diffs, math.abs(-table1[i]+table2[i]))
 end
 
-print(table.concat(diffs, '\n'))
-
 -- sum diffs
 
 local sum = 0
@@ -40,4 +37,24 @@ for i = 1, #diffs do
     sum = sum + diffs[i]
 end
 
-print('\n', 'total sum: ', sum)
+print("answer to part 1")
+print(sum)
+
+
+-- PART 2 -- 
+
+local score = 0 
+
+for k,v in ipairs(table1) do
+    local count = 0 
+    for i,j in ipairs(table2) do 
+        if v == j then 
+            count = count + 1
+        end
+    end
+    print('found', count, 'elements')
+    score = score + count * v
+end
+
+print("answer to part 2")
+print(score)
